0cba9b9588d1966d2e7588d663dd581a61f6840a
Alexey <groznovaa@gmail.comls>
2021-07-18 18:31:22 +0500

    Added class CalculateCatStrengthBeanPostProcessor

---
+++ src/main/java/com/javastart/springcontext/CalculateCatStrengthBeanPostProcessor.java
@@ -0,0 +1,16 @@
+package com.javastart.springcontext;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class CalculateCatStrengthBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        return null;
+    }
+}
