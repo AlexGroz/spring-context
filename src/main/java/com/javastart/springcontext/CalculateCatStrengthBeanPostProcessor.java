e591b2e535cb6f7a3c908a230b548166906e78ee
Alexey <groznovaa@gmail.comls>
2021-07-18 18:47:38 +0500

    Added CalculateCatStrengthBeanPostProcessor and ovveride BeforeInit

--- src/main/java/com/javastart/springcontext/CalculateCatStrengthBeanPostProcessor.java
+++ src/main/java/com/javastart/springcontext/CalculateCatStrengthBeanPostProcessor.java
@@ -3,14 +3,27 @@ package com.javastart.springcontext;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import java.lang.reflect.Field;
+import java.util.Random;
+
 public class CalculateCatStrengthBeanPostProcessor implements BeanPostProcessor {
     @Override
-    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
-        return null;
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Field[] declareFields = bean.getClass().getDeclaredFields();
+        for (Field field : declareFields) {
+            CalculateCatStrength annotation = field.getAnnotation(CalculateCatStrength.class);
+            if (annotation != null) {
+                int minStrength = annotation.minStrength();
+                int maxStrength = annotation.maxStrength();
+                Random random = new Random();
+                int result = minStrength + random.nextInt(maxStrength - minStrength);
+            }
+        }
+        return bean;
     }
 
     @Override
-    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
-        return null;
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
     }
 }
